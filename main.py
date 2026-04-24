563b7dcd8bb41c0d8a9a1f2ac331826e1c2debd2
A3s-Parshav <parshav@a3s.co.in>
2026-04-24 20:59:23 +0530

    add mp3 sound when object detect

--- main.py
+++ main.py
@@ -1,55 +1,48 @@
-from os.path import exists
-
 import cv2 as cv
-from ultralytics import YOLO
-import os
+import pygame
 
-os.makedirs("outputs",exist_ok=True)
+pygame.mixer.init()
+pygame.mixer.music.load("alert.mp3")  # add your sound file
 
 
 class main:
     def video_capture(self, video_path):
 
         cap = cv.VideoCapture(video_path)
-        count =0
-        model = YOLO('yolo26n.pt')
-        face_cascade = cv.CascadeClassifier(
-            cv.data.haarcascades + "haarcascade_frontalface_default.xml"
-        )
-        if face_cascade.empty() & cap.isOpened():
-            print("❌ Face cascade not loaded or video not opened")
-            exit()
-        # if not cap.isOpened():
-        #     print("Video not available")
-        #     return
+
+        if not cap.isOpened():
+            print("❌ Video not opened")
+            return
+
         while True:
             ret, frame = cap.read()
-            result = model.predict(frame ,classes = [0])
-            for box in result[0].boxes:
-                x1,y1,x2,y2 = map(int , box.xyxy[0])
-                person_roi = frame[y1:y2,x1:x2]
+            ret2, frame1 = cap.read()
 
-                gray = cv.cvtColor(person_roi,cv.COLOR_BGR2GRAY)
-                faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-                for (fx,fy,fw,fh) in faces:
-                    fase_crop = person_roi[fy:fy+fh,fx:fx+fw]
-                    filename = f"outputs/{count}.png"
-                    cv.imwrite(filename,fase_crop)
-                    count += 1
-                    cv.rectangle(frame,(x1 +fx , y1+fy),(x1+fx+fw,y1+fy+fh),(0,255,0),2)
+            if not ret or not ret2:
+                break
+
+            diff = cv.absdiff(frame, frame1)
+            gray = cv.cvtColor(diff, cv.COLOR_BGR2GRAY)
+            _, thresh = cv.threshold(gray, 20, 255, cv.THRESH_BINARY)
+
+            contours, _ = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
 
+            for c in contours:
+                if cv.contourArea(c) > 500:
+                    # 🔊 play alarm only when motion detected
+                    if not pygame.mixer.music.get_busy():
+                        pygame.mixer.music.play()
 
-            cv.imshow("frame",frame)
-            if cv.waitKey(1) & 0xFF == ord('q'):
+            cv.imshow("frame", thresh)
+
+            if cv.waitKey(10) == 27:
                 break
 
         cap.release()
         cv.destroyAllWindows()
+
+
 obj = main()
-obj.video_capture('test3.mp4')
-
-
-
-        
+obj.video_capture("test3.mp4")
 
